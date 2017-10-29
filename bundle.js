c602463970f02891408c6a9f43f741d234c78c14
Alex <alexanderhapp@me.com>
2017-10-29 14:57:17 -0700

    Add warrior and brick images

--- bundle.js
+++ bundle.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 5);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -75,10 +75,10 @@ const Util = {
     ctx.fillStyle = fillColor;
     ctx.fillRect(topLeftX, topLeftY, boxWidth, boxHeigth);
   },
-  colorCircle(ctx, centerX, centerY, radius, fillColor) {
+  colorCircle(ctx, pos, radius, fillColor) {
     ctx.fillStyle = fillColor;
     ctx.beginPath();
-    ctx.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
+    ctx.arc(pos[0], pos[1], radius, 0, Math.PI * 2, true);
     ctx.fill();
   }
 };
@@ -91,46 +91,39 @@ module.exports = Util;
 /***/ (function(module, exports, __webpack_require__) {
 
 const Util = __webpack_require__(0);
-const Grids = __webpack_require__(2);
-const Board = __webpack_require__(3);
 
-document.addEventListener("DOMContentLoaded", () => {
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
+class Board {
+  constructor(ctx, squareW, squareH, grid) {
+    this.ctx = ctx;
+    this.squareW = squareW;
+    this.squareH = squareH;
+    this.grid = grid;
+    this.gap = 2;
+    this.cols = grid[0].length;
+    this.rows = grid.length;
+    this.img = document.getElementById("brick");
+  }
 
-  const squareW = 40;
-  const squareH = 40;
+  draw() {
+    for (let eachRow = 0; eachRow < this.rows; eachRow++) {
+      const row = this.grid[eachRow];
+      for (let eachCol = 0; eachCol < row.length; eachCol++) {
+        if (row[eachCol] === 1) {
+          this.ctx.drawImage(
+            this.img,
+            this.squareW * eachCol,
+            this.squareH * eachRow,
+            this.squareW - 2,
+            this.squareH - this.gap
+          );
+          
+        }
+      }
+    }
+  }
+}
 
-  const framesPerSecond = 30;
-
-  const boardGrid = Grids.levelOne();
-
-  const board = new Board(ctx, squareW, squareH, boardGrid);
-
-  const drawAll = () => {
-    Util.colorRect(ctx, 0, 0, canvas.width, canvas.height, "#bec4ce");
-    board.draw();
-  };
-
-  const updateAll = () => {
-    // moveAll();
-    drawAll();
-  };
-
-  let mouseX = 0;
-  let mouseY = 0;
-
-  const updateMousePosition = e => {
-    const rect = canvas.getBoundingClientRect();
-    const root = document.documentElement;
-
-    mouseX = e.clientX - rect.left - root.scrollLeft;
-    mouseY = e.clientY - rect.top - root.scrollTop;
-  };
-
-  canvas.addEventListener("mousemove", updateMousePosition);
-  setInterval(updateAll, 1000 / framesPerSecond);
-});
+module.exports = Board;
 
 
 /***/ }),
@@ -142,7 +135,7 @@ const Grids = {
     return [
       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
       [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
-      [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1],
+      [1, 0, 3, 0, 3, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 3, 1],
       [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1],
       [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
       [1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 2, 0, 1, 0, 0, 0, 0, 0, 1],
@@ -168,36 +161,143 @@ module.exports = Grids;
 
 const Util = __webpack_require__(0);
 
-class Board {
-  constructor(ctx, squareW, squareH, grid) {
-    this.ctx = ctx;
-    this.squareW = squareW;
-    this.squareH = squareH;
-    this.grid = grid;
-    this.gap = 2;
-    this.cols = grid[0].length;
-    this.rows = grid.length;
+class Key {
+  constructor(ctx) {
+    this.pos = [0, 0];
   }
 
   draw() {
-    for (let eachRow = 0; eachRow < this.rows; eachRow++) {
-      const row = this.grid[eachRow];
-      for (let eachCol = 0; eachCol < row.length; eachCol++) {
-        if (row[eachCol] === 1)
-          Util.colorRect(
-            this.ctx,
-            this.squareW * eachCol,
-            this.squareH * eachRow,
-            this.squareW - 2,
-            this.squareH - this.gap,
-            "#e54c0b"
-          );
-      }
-    }
+    Util.colorCircle(this.ctx, this.pos, 20, "#e8f442");
   }
 }
 
-module.exports = Board;
+module.exports = Key;
+
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const Util = __webpack_require__(0);
+
+class Warrior {
+  constructor(ctx, board) {
+    this.ctx = ctx;
+    this.board = board;
+    this.pos = [0, 0];
+    this.width = 50;
+    this.height = 50;
+    this.img = document.getElementById('warrior');
+    this.reset();
+  }
+
+  reset() {
+    for (let eachRow = 0; eachRow < this.board.grid.length; eachRow++) {
+      const row = this.board.grid[eachRow];
+      for (let eachCol = 0; eachCol < row.length; eachCol++) {
+        if (row[eachCol] === 2) {
+          row[eachCol] = 0;
+          this.pos[0] = eachCol * this.board.squareW + this.board.squareW / 2;
+          this.pos[1] = eachRow * this.board.squareH + this.board.squareH / 2;
+        }
+      }
+    }
+  }
+
+  draw() {
+    this.ctx.drawImage(this.img, this.pos[0], this.pos[1], this.width, this.height);
+    // console.log(this.row, this.col);
+    // console.log(this.board.grid[this.row][this.col]);
+  }
+
+  move(dir) {
+    
+    this.pos[0] += dir[0];
+    this.pos[1] += dir[1];
+  }
+}
+
+Warrior.MOVES = {
+  up: [0, -1],
+  left: [-1, 0],
+  down: [0, 1],
+  right: [1, 0]
+};
+
+module.exports = Warrior;
+
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const Util = __webpack_require__(0);
+const Grids = __webpack_require__(2);
+const Board = __webpack_require__(1);
+const Warrior = __webpack_require__(4);
+const Key = __webpack_require__(3);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+
+  const squareW = 40;
+  const squareH = 40;
+
+  const framesPerSecond = 30;
+
+  const boardGrid = Grids.levelOne();
+
+  const board = new Board(ctx, squareW, squareH, boardGrid);
+  const warrior = new Warrior(ctx, board);
+  const key = new Key(ctx, board);
+
+  warrior.reset();
+
+  const drawAll = () => {
+    Util.colorRect(ctx, 0, 0, canvas.width, canvas.height, "#bec4ce");
+    board.draw();
+    warrior.draw();
+  };
+
+  const updateAll = () => {
+    // moveAll();
+    drawAll();
+  };
+
+  let mouseX = 0;
+
+  let mouseY = 0;
+
+  const updateMousePosition = e => {
+    const rect = canvas.getBoundingClientRect();
+    const root = document.documentElement;
+
+    mouseX = e.clientX - rect.left - root.scrollLeft;
+    mouseY = e.clientY - rect.top - root.scrollTop;
+    // warrior.pos = [mouseX, mouseY];wdaas
+  };
+
+  canvas.addEventListener("mousemove", updateMousePosition);
+  document.addEventListener("keydown", e => {
+    switch (e.keyCode) {
+      case 37:
+        warrior.move(Warrior.MOVES.left);
+        break;
+      case 38:
+        warrior.move(Warrior.MOVES.up);
+        break;
+      case 39:
+        warrior.move(Warrior.MOVES.right);
+        break;
+      case 40:
+        warrior.move(Warrior.MOVES.down);
+        break;
+    }
+  });
+  setInterval(updateAll, 1000 / framesPerSecond);
+  warrior.draw();
+});
 
 
 /***/ })
